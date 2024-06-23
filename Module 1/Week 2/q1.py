239b39b430ea2716d1768e689013072d3efd71bf
xngtinh <4401104196@student.hcmue.edu.vn>
2024-06-24 00:38:26 +0700

    Add solution for OOP Python

--- Module 1/Week 2/q1.py
+++
@@ -1,9 +0,0 @@
-num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
-k = 3
-output = []
-
-for i in range(k, len(num_list)+1):
-    max_value = max(num_list[i-3:i])
-    output.append(max_value)
-
-print(output)
---
+++ Module 1/Week 3/q1.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+
+class MySoftmax(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        x_exp = torch.exp(x)
+        partition = x_exp.sum(0, keepdims=True)
+        return x_exp / partition
+
+
+class SoftmaxStable(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        x_max = torch.max(x, dim=0, keepdims=True)
+        x_exp = torch.exp(x - x_max.values)
+        partition = x_exp.sum(0, keepdims=True)
+        return x_exp / partition
